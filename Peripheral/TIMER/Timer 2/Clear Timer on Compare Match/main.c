0e4506f20c9dbadbe1fc5d863951750311feb244
dotai315 <57071897+dotai315@users.noreply.github.com>
2021-12-21 12:58:39 +0700

    Add files via upload

---
+++ Peripheral/TIMER/Timer 2/Clear Timer on Compare Match/main.c
@@ -0,0 +1,61 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sfr_defs.h>
+#include <util/delay.h>
+#include <stdbool.h>
+
+uint8_t cnt;
+
+void GPIO_Config(void);
+void TIMER2_Config(void);
+void TIMER2_On(void);
+void TIMER2_Off(void);
+void TIMER2_SetOCR2A(unsigned char value);
+
+int main()
+{
+	cnt = 0;
+	GPIO_Config();
+	TIMER2_Config();
+
+	sei();
+
+	while(1)
+	{
+		for(uint8_t i = 0; i < 255; i++)
+		{
+			TIMER2_SetOCR2A(i);
+			_delay_ms(8);
+		}
+
+		for(uint8_t i = 255; i > 1; i--)
+		{
+			TIMER2_SetOCR2A(i);
+			_delay_ms(8);
+		}
+
+	}
+	return 0;
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+	PORTB ^= _BV(PB5);
+}
+
+void GPIO_Config()
+{
+	DDRB |=  _BV(PB5);
+}
+
+void TIMER2_Config()
+{
+	TCCR2A |= _BV(WGM21);
+	TIMSK2 |= _BV(OCIE2A);
+	TCCR2B |= _BV(CS20);
+}
+
+void TIMER2_SetOCR2A(unsigned char value)
+{
+	OCR2A = value;
+}
